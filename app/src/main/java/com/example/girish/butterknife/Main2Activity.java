366a4b99e5faf1b3ba06612ea36bd66260a19a7a
Girish <girish12ramesh@gmail.com>
2017-10-01 12:29:16 -0700

    feat : Toolbar Added a tool bar

--- app/src/main/java/com/example/girish/butterknife/Main2Activity.java
+++ app/src/main/java/com/example/girish/butterknife/Main2Activity.java
@@ -1,11 +1,14 @@
 package com.example.girish.butterknife;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import static com.example.girish.butterknife.R.id.toolbar;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -24,6 +27,18 @@ public class Main2Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        toolbar.setNavigationIcon(R.drawable.back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v){
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
     }
+
 
 }
